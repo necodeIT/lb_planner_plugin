16955cd92ac2d8ef2bf655f28120302828df0164
Riedler <git@riedler.wien>
2024-08-19 21:16:26 +0200

    fix: added single blank line after boilerplate comment

--- lbplanner/classes/helpers/plan_helper.php
+++ lbplanner/classes/helpers/plan_helper.php
@@ -13,6 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * Provides helper classes for any tables related with the planning function of the app
  *
