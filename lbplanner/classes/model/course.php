c763a1fb83b5fb0ae6d35f7e9f8db61bafd29451
Riedler <git@riedler.wien>
2024-08-23 17:45:34 +0200

    refactor: harmonized everything with the new course model object

--- lbplanner/classes/model/course.php
+++ lbplanner/classes/model/course.php
@@ -27,6 +27,7 @@ namespace local_lbplanner\model;
 
 use external_single_structure;
 use external_value;
+use local_lbplanner\helpers\course_helper;
 
 /**
  * Model class for course
@@ -35,7 +36,7 @@ class course {
     /**
      * @var int $id course ID
      */
-    public int $id;
+    private int $id;
     /**
      * @var int $courseid the moodle-internal ID of the course
      */
@@ -50,9 +51,11 @@ class course {
      */
     public string $shortname;
     /**
-     * @var string $color the color for this course
-     * maximum size: 10 chars
-     * TODO: what is the format of this ?? RRGGBB is just 6 chars
+     * @var string $fullname the full name of this course
+     */
+    private ?string $fullname;
+    /**
+     * @var string $color the color for this course as #RRGGBB
      */
     public string $color;
     /**
@@ -66,26 +69,24 @@ class course {
      * @param int $courseid ID of the moodle course
      * @param int $userid ID of the user these settings are for
      * @param string $shortname the short name for this course
-     * @param string $color the color for this course
+     * @param string $color the color for this course as #RRGGBB
      * @param bool $enabled whether the course is enabled
      */
     public function __construct(int $id, int $courseid, int $userid, string $shortname, string $color, bool $enabled) {
         $this->id = $id;
         $this->courseid = $courseid;
         $this->userid = $userid;
-        assert(strlen($shortname) <= 5);
-        assert(strlen($shortname) > 0);
-        $this->shortname = $shortname;
-        assert(strlen($color) <= 10);
-        // TODO: check color format.
-        $this->color = $color;
+        $this->set_shortname($shortname);
+        $this->set_color($color);
         $this->enabled = $enabled;
+        $this->fullname = null;
     }
 
     /**
      * Takes data from DB and makes a new Course out of it
      *
      * @return object a representation of this course and its data
+     * @throws \AssertionError
      */
     public static function from_db(object $obj): self {
         assert($obj->enabled === 0 || $obj->enabled === 1);
@@ -93,19 +94,101 @@ class course {
     }
 
     /**
+     * Mark the object as freshly created and sets the new ID
+     * @param int $id the new ID after insertint into the DB
+     * @throws \AssertionError
+     */
+    public function set_fresh(int $id) {
+        assert($this->id === 0);
+        assert($id !== 0);
+        $this->id = $id;
+    }
+
+    /**
+     * sets the color as #RRGGBB
+     * @param string $color the color
+     * @throws \AssertionError
+     */
+    public function set_color(string $color) {
+        // TODO: check color format.
+        assert(strlen($color) <= 10);
+        $this->color = $color;
+    }
+
+    /**
+     * sets the shortname
+     * @param string $shortname the shortname
+     * @throws \AssertionError
+     */
+    public function set_shortname(string $shortname) {
+        assert(strlen($shortname) <= 5);
+        assert(strlen($shortname) > 0);
+        $this->shortname = $shortname;
+    }
+
+    /**
+     * sets whether the course is enabled
+     * @param bool $enabled whether to enable the course
+     */
+    public function set_enabled(bool $enabled) {
+        $this->enabled = $enabled;
+    }
+
+    /**
+     * sets the cached fullname (mainly for deduplicating DB requests)
+     * @param string $fullname the cached fullname
+     */
+    public function set_fullname(string $fullname) {
+        // If we already have a fullname stored and it's different, then we've found data inconsistency.
+        assert($this->fullname === null || $this->fullname === $fullname);
+        $this->fullname = $fullname;
+    }
+
+    /**
+     * get the cached fullname
+     * @return string the cached fullname
+     */
+    public function get_fullname(): string {
+        if ($this->fullname === null) {
+            $this->fullname = course_helper::get_fullname($this->courseid);
+        }
+
+        return $this->fullname;
+    }
+
+    /**
+     * Prepares a string to be eligible for shortname
+     * @param string $shortname the shortname to be prepared
+     * @return string the prepared shortname
+     */
+    public static function prepare_shortname(string $shortname): string {
+        if (strpos($shortname, ' ') !== false) {
+            $shortname = substr($shortname, 0, strpos($shortname, ' '));
+        }
+        if (strlen($shortname) >= 5) {
+            $shortname = substr($shortname, 0, 5);
+        }
+        return strtoupper($shortname);
+    }
+
+    /**
      * Prepares data for the DB endpoint.
+     * doesn't set ID if it's 0
      *
      * @return object a representation of this course and its data
      */
     public function prepare_for_db(): object {
         $obj = new \stdClass();
 
-        $obj->id = $this->id;
         $obj->courseid = $this->courseid;
         $obj->userid = $this->userid;
         $obj->shortname = $this->shortname;
         $obj->color = $this->color;
         $obj->enabled = $this->enabled ? 1:0; // The DB uses int instead of bool here.
+
+        if ($this->id !== 0) {
+            $obj->id = $this->id;
+        }
 
         return $obj;
     }
@@ -120,6 +203,7 @@ class course {
             'id' => $this->id,
             'courseid' => $this->courseid,
             'userid' => $this->userid,
+            'name' => $this->get_fullname(),
             'shortname' => $this->shortname,
             'color' => $this->color,
             'enabled' => $this->enabled ? 1:0, // Moodle's API uses int instead of bool.
@@ -137,8 +221,9 @@ class course {
                 'id' => new external_value(PARAM_INT, 'course ID'),
                 'courseid' => new external_value(PARAM_INT, 'the moodle-internal ID of the course'),
                 'userid' => new external_value(PARAM_INT, 'the user for whom these course settings are for'),
+                'name' => new external_value(PARAM_TEXT, 'the full name of this course'),
                 'shortname' => new external_value(PARAM_TEXT, 'the short name of this course for this user (maximum size: 5 chars)'),
-                'color' => new external_value(PARAM_TEXT, 'the color for this course (maximum size: 10 chars)'), // TODO: describe format
+                'color' => new external_value(PARAM_TEXT, 'the color for this course as #RRGGBB'),
                 'enabled' => new external_value(PARAM_BOOL, 'whether the user wants to see this course'),
             ]
         );
--- lbplanner/services/courses/get_all_courses.php
+++ lbplanner/services/courses/get_all_courses.php
@@ -16,15 +16,12 @@
 
 namespace local_lbplanner_services;
 
-use coding_exception;
-use dml_exception;
 use external_api;
 use external_function_parameters;
 use external_multiple_structure;
-use external_single_structure;
-use external_value;
+
 use local_lbplanner\helpers\course_helper;
-use moodle_exception;
+use local_lbplanner\model\course;
 
 /**
  * Get all the courses of the current year.
@@ -46,55 +43,14 @@ class courses_get_all_courses extends external_api {
 
     /**
      * Get all the courses of the current year.
-     * @throws coding_exception
-     * @throws dml_exception
-     * @throws moodle_exception
      */
     public static function get_all_courses(): array {
-        global $DB, $USER;
-
-        $userid = $USER->id;
-
-        $courses = enrol_get_my_courses();
-        // Remove Duplicates.
-        $courses = array_unique($courses, SORT_REGULAR);
-        // Check this out: https://www.youtube.com/watch?v=WmdAk2zyQkU .
-        $catgirls = [];
-
+        $courses = course_helper::get_all_lbplanner_courses();
+        $results = [];
         foreach ($courses as $course) {
-            $courseid = $course->id;
-            $name = $course->fullname;
-            $shortname = $course->shortname;
-            // Check if the shortname contains a space.
-            if (strpos($shortname, ' ') !== false) {
-                    $shortname = substr($shortname, 0, strpos($shortname, ' '));
-            }
-            if (strlen($shortname) >= 5) {
-                    $shortname = substr($shortname, 0, 5);
-            }
-            // Check if the course is from the current year.
-            if (course_helper::check_current_year($courseid)) {
-                    continue;
-            }
-            // Check if the course is already in the LB Planner database.
-            if ($DB->record_exists(course_helper::LBPLANNER_COURSE_TABLE, ['courseid' => $courseid, 'userid' => $userid])) {
-                $fetchedcourse = course_helper::get_lbplanner_course($courseid, $userid);
-            } else {
-                // IF not create an Object to be put into the LB Planner database.
-                $fetchedcourse = (object) [
-                    'courseid' => $courseid,
-                    'color' => course_helper::COLORS[array_rand(course_helper::COLORS)],
-                    'shortname' => strtoupper($shortname),
-                    'enabled' => course_helper::DISABLED_COURSE,
-                    'userid' => $userid,
-                ];
-                $DB->insert_record(course_helper::LBPLANNER_COURSE_TABLE, $fetchedcourse);
-            }
-            // Add name to fetched Course.
-            $fetchedcourse->name = $name;
-            $catgirls[] = $fetchedcourse;
+            array_push($results, $course->prepare_for_api());
         }
-            return $catgirls;
+        return $results;
     }
 
     /**
@@ -103,15 +59,7 @@ class courses_get_all_courses extends external_api {
      */
     public static function get_all_courses_returns(): external_multiple_structure {
         return new external_multiple_structure(
-        new external_single_structure(
-            [
-                'courseid' => new external_value(PARAM_INT, 'The id of the course'),
-                'color' => new external_value(PARAM_TEXT, 'The color of the course in HEX'),
-                'name' => new external_value(PARAM_TEXT, 'The name of the course'),
-                'shortname' => new external_value(PARAM_TEXT, 'The shortname of the course'),
-                'enabled' => new external_value(PARAM_BOOL, 'Whether the course is enabled or not'),
-            ]
-        )
+            course::api_structure()
         );
     }
 }
