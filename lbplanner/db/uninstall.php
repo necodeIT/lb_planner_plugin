d5f4e81cf7793dbc8f083018fe2ca58166bf5bee
Riedler <git@riedler.wien>
2025-01-18 00:22:03 +0100

    fix: rework install/uninstall/upgrade handlers

---
+++ lbplanner/db/uninstall.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of local_lbplanner.
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Contains some stuff for when the plugin gets uninstalled.
+ * Upgrading the plugin shouldn't trigger this code.
+ *
+ * @package local_lbplanner
+ * @subpackage db
+ * @copyright 2024 NecodeIT
+ * @license https://creativecommons.org/licenses/by-nc-sa/4.0/ CC-BY-NC-SA 4.0 International or later
+ */
+
+use local_lbplanner\helpers\config_helper;
+
+
+/**
+ * Runs when plugin is uninstalled
+ */
+function xmldb_local_lbplanner_uninstall() {
+    config_helper::remove_customfield();
+}
+
