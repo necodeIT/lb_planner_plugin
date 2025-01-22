76058259e163820791092df4d1f9dabbf6c270c3
Riedler <git@riedler.wien>
2025-01-22 14:35:10 +0100

    feat: allow ekenabled as parameter in get_all_modules

--- lbplanner/services/modules/get_all_modules.php
+++ lbplanner/services/modules/get_all_modules.php
@@ -16,7 +16,7 @@
 
 namespace local_lbplanner_services;
 
-use core_external\{external_api, external_function_parameters, external_multiple_structure};
+use core_external\{external_api, external_function_parameters, external_multiple_structure, external_value};
 use local_lbplanner\helpers\{modules_helper, plan_helper, course_helper};
 use local_lbplanner\model\module;
 
@@ -34,7 +34,15 @@ class modules_get_all_modules extends external_api {
      * @return external_function_parameters
      */
     public static function get_all_modules_parameters(): external_function_parameters {
-        return new external_function_parameters([]);
+        return new external_function_parameters([
+            'ekenabled' => new external_value(
+                PARAM_BOOL,
+                'Whether to include ek modules',
+                VALUE_DEFAULT,
+                false,
+                NULL_NOT_ALLOWED
+            ),
+        ]);
     }
 
     /**
@@ -42,15 +50,17 @@ class modules_get_all_modules extends external_api {
      *
      * @return array the modules
      */
-    public static function get_all_modules(): array {
+    public static function get_all_modules(bool $ekenabled): array {
         global $USER;
+        self::validate_parameters(
+            self::get_all_modules_parameters(),
+            ['ekenabled' => $ekenabled]
+        );
 
         $modules = [];
 
         $courses = course_helper::get_all_lbplanner_courses();
         $planid = plan_helper::get_plan_id($USER->id);
-        $plan = plan_helper::get_plan($planid);
-        $ekenabled = $plan["enableek"];
 
         foreach ($courses as $course) {
             if (!$course->enabled) {
