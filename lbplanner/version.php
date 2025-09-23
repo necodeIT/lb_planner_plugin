bde193f20edb4da38d34eab575e1a9c3bbd79fc0
Riedler <git@riedler.wien>
2025-09-23 18:05:01 +0200

    fix: actually bumped version
    goddamned automatic tools that only do half of what they claim

--- lbplanner/version.php
+++ lbplanner/version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->requires = 2024042200.00; // Require Moodle >=4.4.0.
 $plugin->maturity = MATURITY_BETA;
 $plugin->component = 'local_lbplanner';
-$plugin->release = '1.0.2';
+$plugin->release = '1.1.0';
 $plugin->version = 202509120001;
 $plugin->dependencies = [
     // Depend upon version 2023110600 of local_modcustomfields.
